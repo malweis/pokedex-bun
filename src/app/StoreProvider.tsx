db183ed2ab9795a97f5a06f790d32ee410cf5605
Federico Luis Molas Sanchez <fmolas@roshka.com>
2024-05-21 22:48:47 -0400

    feat: redux persist implementation

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,15 +1,19 @@
-'use client'
-import { store } from '@/store/store'
-import { useRef } from 'react'
-import { Provider } from 'react-redux'
-
+"use client";
+import { persistor, store } from "@/store/store";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function StoreProvider({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
- 
-
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import pokemonReducer from './pokemon/pokemonSlice'
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-export const store =
-   configureStore({
-    reducer: {
-        pokemon : pokemonReducer,
-    },
-  })
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    persistStore,
+    persistReducer,
+  } from 'redux-persist';
+
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    };
+  
+  const rootReducer = combineReducers({
+    pokemon : pokemonReducer,
+  
+  });
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  });
 
 
 // Infer the type of makeStore
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
+export  const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
